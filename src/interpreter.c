88be2009579380df3cbd036af71eb902d3894459
yanruijie902136 <yanruijie902136@gmail.com>
2025-01-26 20:50:00 +0800

    codecrafters submit [skip ci]

--- src/interpreter.c
+++ src/interpreter.c
@@ -3,12 +3,18 @@
 
 static const Object *evaluateExpr(const Expr *expr);
 
+static const Object *evaluateGroupingExpr(const GroupingExpr *groupingExpr) {
+        return evaluateExpr(groupingExpr->expression);
+}
+
 static const Object *evaluateLiteralExpr(const LiteralExpr *literalExpr) {
         return literalExpr->value;
 }
 
 static const Object *evaluateExpr(const Expr *expr) {
         switch (expr->type) {
+        case EXPR_GROUPING:
+                return evaluateGroupingExpr(expr->data);
         case EXPR_LITERAL:
                 return evaluateLiteralExpr(expr->data);
         }
